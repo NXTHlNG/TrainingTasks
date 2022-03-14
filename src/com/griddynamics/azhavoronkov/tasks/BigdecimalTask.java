dfa1fe059df726899e55b4fc2eb74890a4481fc7
NXTHING <nxthing.az@gmail.com>
2022-03-15 01:05:29 +0400

    refactored

--- src/com/griddynamics/azhavoronkov/tasks/BigdecimalTask.java
+++ src/com/griddynamics/azhavoronkov/tasks/BigdecimalTask.java
@@ -3,8 +3,6 @@ package com.griddynamics.azhavoronkov.tasks;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class BigdecimalTask {
     public static void main(String[] args) {
@@ -17,10 +15,7 @@ public class BigdecimalTask {
         sc.close();
 
         Arrays.stream(strings)
-                .collect(Collectors.toMap(Function.identity(), BigDecimal::new))
-                .entrySet()
-                .stream()
-                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                .forEach(e -> System.out.println(e.getKey()));
+                .sorted((s1, s2) -> new BigDecimal(s2).compareTo(new BigDecimal(s1)))
+                .forEach(System.out::println);
     }
 }
